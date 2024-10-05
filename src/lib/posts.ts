084a05277814800cd98e4d1c9957decf73c8c034
Bahoz <julienbraultcherif@gmail.com>
2024-10-06 00:18:40 +0200

    test fix error loading content article blog

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -13,7 +13,10 @@ const PostSchema = z.object({
 });
 
 // Ajout de `content` au type `Post`
-export type Post = z.infer<typeof PostSchema> & { slug: string; content: string };
+export type Post = z.infer<typeof PostSchema> & {
+  slug: string;
+  content: string;
+};
 
 export const getPosts = async () => {
   const files = await fs.readdir(postsDirectory);
@@ -22,29 +25,34 @@ export const getPosts = async () => {
   const posts: Post[] = [];
   for await (const fileName of fileNames) {
     const fullPath = path.join(postsDirectory, fileName);
-    const fileContent = await fs.readFile(fullPath, "utf-8");
-    const frontmatter = matter(fileContent);
+    try {
+      const fileContent = await fs.readFile(fullPath, "utf-8");
+      const frontmatter = matter(fileContent);
 
-    const safeData = PostSchema.safeParse(frontmatter.data);
+      const safeData = PostSchema.safeParse(frontmatter.data);
 
-    if (!safeData.success) {
-      console.error(
-        `Error parsing file: ${fileName} - ${safeData.error.issues
-          .map((i) => i.message)
-          .join(", ")}`
-      );
+      if (!safeData.success) {
+        console.error(
+          `Error parsing file: ${fileName} - ${safeData.error.issues
+            .map((i) => i.message)
+            .join(", ")}`
+        );
+        continue;
+      }
+
+      if (!safeData.data.published && process.env.NODE_ENV !== "development") {
+        continue;
+      }
+
+      posts.push({
+        ...safeData.data,
+        slug: fileName.replace(/^\d+-/, "").replace(".mdx", ""),
+        content: frontmatter.content, // Ajout du contenu ici
+      });
+    } catch (error) {
+      console.error(`Error reading file: ${fileName} - ${error}`);
       continue;
     }
-
-    if (!safeData.data.published && process.env.NODE_ENV !== "development") {
-      continue;
-    }
-
-    posts.push({
-      ...safeData.data,
-      slug: fileName.replace(/^\d+-/, "").replace(".mdx", ""),
-      content: frontmatter.content, // Ajout du contenu ici
-    });
   }
 
   return posts;
