59f13943c342fbc8fcd8e7b7099dd3f448ab3eff
Bahoz <julienbraultcherif@gmail.com>
2024-10-06 00:29:02 +0200

    change route api

--- app/api/posts/[slug]/route.ts
+++ app/api/posts/[slug]/route.ts
@@ -2,11 +2,12 @@ import { getPost } from "@/lib/posts";
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
-export const GET = async (
-  request: NextRequest,
-  props: { params: { slug: string } }
-) => {
-  const { slug } = props.params;
+export const GET = async (request: NextRequest) => {
+  const slug = request.nextUrl.pathname.split("/").pop() ?? ""; // Assurer que slug est une chaîne vide par défaut
+
+  if (!slug) {
+    return NextResponse.json({ error: "Slug not found" }, { status: 400 });
+  }
 
   const post = await getPost(slug);
 
--- app/posts/[slug]/getUrl.ts
+++ app/posts/[slug]/getUrl.ts
@@ -1,7 +1,11 @@
 export const getUrl = () => {
+/*************  ✨ Codeium Command 🌟  *************/
     if (process.env.NODE_ENV === "production") {
-      return "https://codelynx-blog-clone-r22gtnt97-bahooozs-projects.vercel.app/";
-    }
+      return (
+        process.env.VERCEL_URL ||
+        "https://codelynx-blog-clone-r22gtnt97-bahooozs-projects.vercel.app/"
+      );
+    }/******  e763d24d-02c4-4e91-8fd5-1af3fa6e1b5f  *******/
   
     return "http://localhost:3000";
   };
