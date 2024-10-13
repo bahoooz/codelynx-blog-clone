c8aaed657aac6493d22d55bd11ff11e3b33e4234
Bahoz <julienbraultcherif@gmail.com>
2024-10-13 21:54:11 +0200

    test without Suspense and options plugins in Mdx component

--- src/features/mdx/Mdx.tsx
+++ src/features/mdx/Mdx.tsx
@@ -1,20 +1,20 @@
 import { MDXRemote } from "next-mdx-remote-client/rsc";
-import { Suspense } from "react";
+// import { Suspense } from "react";
 import { MDX_COMPONENTS } from "./mdx-components";
-import { rehypePlugin } from "./mdx-plugin";
+// import { rehypePlugin } from "./mdx-plugin";
 
 export const Mdx = ({ children }: { children: string }) => {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    // <Suspense fallback={<div>Loading...</div>}>
       <MDXRemote
         source={children}
-        options={{
-          mdxOptions: {
-            rehypePlugins: rehypePlugin,
-          },
-        }}
+        // options={{
+        //   mdxOptions: {
+        //     rehypePlugins: rehypePlugin,
+        //   },
+        // }}
         components={MDX_COMPONENTS}
       />
-    </Suspense>
+    // </Suspense>
   );
 };
